da139e11faffa1d573b4c9a25eefc5d54b3ea6e4
Severalbuttons88 <44512019+severalbuttons88@users.noreply.github.com>
2023-03-20 00:01:07 -0500

    Initialize

---
+++ index.js
@@ -0,0 +1,194 @@
+const tree = (array) => {
+  let treeRoot = null;
+
+  const deleteValue = (value) => {
+    treeRoot = deleteRec(treeRoot, value);
+  };
+  const getTreeRoot = () => {
+    return treeRoot;
+  };
+  const levelOrder = (callbackFunc) => {
+    const queue1 = queue();
+    let currentRoot = getTreeRoot();
+    while (currentRoot !== null) {
+      
+    }
+
+
+  };
+
+  const find = (value, currentNode = treeRoot) => {
+    while (value != currentNode.getData()) {
+      if (currentNode !== null) {
+        if (value < currentNode.getData()) {
+          currentNode = currentNode.getLeftChild();
+        } else {
+          currentNode = currentNode.getRightChild();
+        }
+        if (currentNode === null) {
+          return null;
+        }
+      }
+    }
+    return currentNode;
+  };
+  const deleteRec = (root, value) => {
+    if (root == null) return root;
+    if (value < root.getData()) {
+      root.setLeftChild(deleteRec(root.getLeftChild(), value));
+    } else if (value > root.getData()) {
+      root.setRightChild(deleteRec(root.getRightChild(), value));
+    } else {
+      if (root.getLeftChild() == null) {
+        return root.getRightChild();
+      } else if (root.getRightChild() == null) {
+        return root.getLeftChild();
+      }
+      root.setData(minValue(root.getRightChild()));
+      root.setRightChild(deleteRec(root.getRightChild(), root.getData()));
+    }
+    return root;
+  };
+  function minValue(root) {
+    let minv = root.getData();
+    while (root.getLeftChild() != null) {
+      minv = root.getLeftChild.getData();
+      root = root.getLeftChild();
+    }
+    return minv;
+  }
+  function mergeArray(array) {
+    if (array.length <= 1) {
+      return array;
+    }
+
+    let cutPoint = Math.floor(array.length / 2);
+
+    let leftArray = mergeArray(array.slice(0, cutPoint));
+    let rightArray = mergeArray(array.slice(cutPoint));
+    return merge(leftArray, rightArray);
+  }
+
+  function merge(leftArray, rightArray) {
+    let arr = [];
+    while (leftArray.length > 0 && rightArray.length > 0) {
+      const smallerArray =
+        leftArray[0] < rightArray[0] ? leftArray : rightArray;
+      const addItem = smallerArray.shift();
+      arr.push(addItem);
+    }
+    return arr.concat(leftArray, rightArray);
+  }
+  let sortedArray = mergeArray(array);
+  const buildTree = (array, start = 0, end = sortedArray.length) => {
+    if (start > end) return null;
+    let mid = parseInt((start + end) / 2);
+    if (array[mid] === undefined) return null;
+
+    let root = node();
+    root.setData(array[mid]);
+    root.setLeftChild(buildTree(array, start, mid - 1));
+    root.setRightChild(buildTree(array, mid + 1, end));
+    treeRoot = root;
+    return root;
+  };
+  const getArray = () => {
+    return sortedArray;
+  };
+
+  const insert = (value, nodeRoot, newNode = node()) => {
+    newNode.setData(value);
+    console.log(nodeRoot);
+    if (newNode.getData() < nodeRoot.getData()) {
+      if (nodeRoot.getLeftChild() === null) {
+        nodeRoot.setLeftChild(newNode);
+      } else {
+        insert(value, nodeRoot.getLeftChild(), newNode);
+      }
+    } else {
+      if (nodeRoot.getRightChild() === null) {
+        nodeRoot.setRightChild(newNode);
+      } else {
+        insert(value, nodeRoot.getRightChild(), newNode);
+      }
+    }
+  };
+  const queue = () => {
+    let queueStack = [];
+    const isEmpty = () => {
+      if (queueStack.length === 0) {
+        return true;
+      }
+      return false;
+    }
+    const getQueue = () => {
+      return queueStack;
+    };
+    const enqueue = (value) => {
+      queueStack.push(value);
+    };
+    const dequeue = (value) => {
+      return queueStack.pop();
+    };
+    return { getQueue, enqueue, dequeue, isEmpty };
+  };
+  const node = (nodeData) => {
+    let data = nodeData;
+    let leftChild = null;
+    let rightChild = null;
+
+    const setData = (nodeData) => {
+      data = nodeData;
+    };
+    const getData = () => {
+      return data;
+    };
+    const setLeftChild = (child) => {
+      leftChild = child;
+    };
+    const setRightChild = (child) => {
+      rightChild = child;
+    };
+    const getLeftChild = () => {
+      return leftChild;
+    };
+    const getRightChild = () => {
+      return rightChild;
+    };
+    return {
+      setData,
+      getData,
+      setLeftChild,
+      setRightChild,
+      getLeftChild,
+      getRightChild,
+    };
+  };
+  return { buildTree, getArray, insert, deleteValue, getTreeRoot, find };
+};
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node.getRightChild() !== null) {
+    prettyPrint(
+      node.getRightChild(),
+      `${prefix}${isLeft ? "│   " : "    "}`,
+      false
+    );
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.getData()}`);
+  if (node.getLeftChild() !== null) {
+    prettyPrint(
+      node.getLeftChild(),
+      `${prefix}${isLeft ? "    " : "│   "}`,
+      true
+    );
+  }
+};
+let testArray = [1, 3, 6, 7, 8, 10, 11, 15, 20, 100];
+let unsortTest = [5, 6, 8, 9, 100, 60, 200];
+let tree1 = tree(unsortTest);
+let rootNode = tree1.buildTree(tree1.getArray());
+console.log(rootNode.getRightChild().getData());
+tree1.insert(35, rootNode);
+
+prettyPrint(rootNode);
+console.log(tree1.find(9).getData());
